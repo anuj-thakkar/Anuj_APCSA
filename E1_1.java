b85d13fffa52dcff2f391928c09ca4ac4e81b7dc
Anuj Thakkar <51032323+anujthakkar2001@users.noreply.github.com>
2019-09-22 13:44:40 -0500

    Add files via upload

---
+++ E1_1.java
@@ -0,0 +1,8 @@
+public class E1_1
+{
+    public static void main(String[]args)
+    {
+        System.out.print("Welcome to Central High");
+        System.out.println("I love Kanye");
+    }
+}    
---
+++ E1_8.java
@@ -0,0 +1,11 @@
+public class E1_8
+{
+    public static void main(String[]args)
+    {
+        System.out.println("-- --");
+        System.out.println(" *  * ");
+        System.out.println("   >   ");
+        System.out.println("-----");
+    }
+}
+     
